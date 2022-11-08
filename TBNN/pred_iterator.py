9bf8f102b49dfdd98a5e7112b890c4a6e65ea819
AnthonyCHM <anthony.man@postgrad.manchester.ac.uk>
2022-11-08 10:01:25 +0000

    Added Cx, Cy and additional inputs 08/11/2022

--- TBNN/pred_iterator.py
+++ TBNN/pred_iterator.py
@@ -1,138 +1,151 @@
-from preprocessor import load_data
+from calculator import PopeDataProcessor
+from core import NetworkStructure, Tbnn, DataLoader, TbnnTVT
+from preprocessor import load_data, scalar_basis_manager, DataSplitter
 from results_writer import write_bij_results, init_log, errors_list_init, write_error_means_csv, write_trial_rmse_csv, \
     write_test_truth_bij, write_param_txt, write_param_csv
-from calculator import PopeDataProcessor
-import case_dicts
+
 import torch
 import numpy as np
 import random
-from core import NetworkStructure, Tbnn, DataLoader, TbnnTVT
 
 
-def preprocessing(enforce_realiz, num_realiz_its, database_name, n_skiprows, num_tensor_basis):
+def preprocessing(database, num_input_markers, num_zonal_markers, incl_p_invars, incl_tke_invars, incl_input_markers,
+                  incl_zonal_markers, rho, num_tensor_basis, enforce_realiz, num_realiz_its):
     """
-    Preprocess the CFD data, which consists of the following steps:
-    1) Load in data using load_channel_data function
+    Preprocess the CFD data:
+    1) Load in data using load_data function
     2) Calculate Sij and Rij for the RANS data using calc_Sij_Rij function
-    3) Calculate invariants for the RANS data using calc_scalar_basis function
+    3) Calculate Sij and Rij invariants for the RANS data using calc_scalar_basis function
     4) Calculate tensor basis for the RANS data using calc_tensor_basis function
-    5) Calculate Reynolds stress anisotropy for the LES/DNS/experimental data using calc_output function
-    6) If enforce_realizability is True, enforce realizability in y using make_realizable function
-    :param enforce_realiz: Boolean for enforcing realizability in y
-    :param num_realiz_its: Number of iterations for enforcing realizability in y
-    :param database_name: Name of the data file containing the CFD data
-    :param n_skiprows: Number of rows at top of data file which this code should skip for reading
+    5) Calculate Reynolds stress anisotropy bij for the LES/DNS/experimental data using calc_output function
+    6) If enforce realizability is True, enforce realizability in bij using make_realizable function
+
+    :param enforce_realiz: boolean for enforcing realizability in y
+    :param num_realiz_its: number of iterations for enforcing realizability in y
+    :param num_tensor_basis: number of tensor basis for bij prediction
+
     :return: invariants x, tensor basis tb and Reynolds stress anisotropy y, for all of the CFD data
     """
 
     # Load in data
-    k, eps, grad_u, stresses = load_data(database_name=database_name, n_skiprows=n_skiprows)
+    Cx, Cy, k, eps, grad_u, grad_p, u, grad_k, input_markers, tauij = \
+        load_data(database, num_input_markers, num_zonal_markers=num_zonal_markers, pressure_tf=incl_p_invars,
+                  tke_tf=incl_tke_invars, input_markers_tf=incl_input_markers, zonal_markers_tf=incl_zonal_markers)
     print("Data loading complete")
 
     # Calculate inputs and outputs
     data_processor = PopeDataProcessor()
-    Sij, Rij = data_processor.calc_Sij_Rij(grad_u, k, eps)  # Mean strain rate tensor k/eps*Sij,
-                                                            # mean rotation rate tensor k/eps*Rij
-    x = data_processor.calc_scalar_basis(Sij, Rij, is_train=True)  # Scalar basis # to do: Check this
+    Sij, Rij, x = scalar_basis_manager(data_processor, k, eps, grad_u, rho, u, grad_p, grad_k,
+                                       incl_p_invars=incl_p_invars, incl_tke_invars=incl_tke_invars)
+    num_inputs = x.shape[1]
     tb = data_processor.calc_tensor_basis(Sij, Rij, num_tensor_basis)  # Tensor basis
-    y = data_processor.calc_output(stresses)  # Anisotropy tensor
+    y = data_processor.calc_output(tauij)  # Anisotropy tensor bij
     print("x, tb and y calculations complete")
 
     # Enforce realizability
     if enforce_realiz is True:
         for i in range(num_realiz_its):
-            y = PopeDataProcessor.make_realizable(y)
+            y = data_processor.make_realizable(y)
         print("Realizability enforced")
 
-    return x, tb, y
+    return Cx, Cy, x, tb, y, num_inputs
 
 
 def set_seed(seed):
+    """
+    Set random seeds for all operations with seed parameter. This ensures the results are reproducible.
+    :param seed: integer between 1 and the value of maximum seed runs
+    """
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
-    return
 
 
-def split_database(x, tb, y, train_list, valid_list, test_list, seed, train_valid_rand_split, train_test_rand_split,
-                   train_valid_split_frac, train_test_split_frac, num_tensor_basis, database_name):
+def split_database(Cx, Cy, x, tb, y, train_list, valid_list, test_list, seed, train_valid_rand_split,
+                   train_test_rand_split, train_valid_split_frac, train_test_split_frac, num_tensor_basis, case_dict):
     """
-    Split the data using your chosen data-splitting method
-    :param x: scalar invariants
+    Split data using the random or specified data-splitting method. The random method randomly allocates (x, tb, y)
+    data for TVT. The specified method allocates data from specific cases for TVT according to the TVT lists.
+    TVT = training, validation and testing.
+
+    :param x: scalar invariants of Sij and Rij
     :param tb: tensor basis
     :param y: Reynolds stress anisotropy
-    :param seed: Current random seed for reproducible database splitting
-    :param train_list: list of cases for training (use if random_split = False)
-    :param test_list: list of cases for testing (use if random_split = False)
-    :param train_test_rand_split: Boolean for enforcing random train-test splitting of data (True) or specifying certain cases
-    for training and testing (False)
-    :param train_test_split_frac: fraction of all CFD data to use for training data (use if random_split = True)
-    :return: inputs, tb and outputs for the training and test sets
+    :param train_list: list of cases for training (use if train_test_rand_split = False)
+    :param valid_list: list of cases for validation (use if train_valid_rand_split = False)
+    :param test_list: list of cases for testing (use if train_test_rand_split = False)
+    :param seed: current random seed for reproducible database splitting
+    :param train_valid_rand_split: boolean for enforcing random train-valid splitting of training data
+    :param train_test_rand_split: boolean for enforcing random train-test splitting of all data
+    :param train_valid_split_frac: fraction of training data to use for actual training,
+    the rest is used for validation (use if train_valid_rand_split = True)
+    :param train_test_split_frac: fraction of all data to use for training data (use if train_test_rand_split = True)
+    :param num_tensor_basis: number of tensor basis for bij prediction
+
+    :return: inputs, tensor basis and outputs for the TVT data sets
     """
 
     # Split database randomly if random split = True
     if train_test_rand_split is True:
-        x_train, tb_train, y_train, x_test, tb_test, y_test = \
-            PopeDataProcessor.random_split(x, tb, y, fraction=train_test_split_frac, seed=seed)
+        Cx_train, Cy_train, x_train, tb_train, y_train, Cx_test, Cy_test, x_test, tb_test, y_test = \
+            DataSplitter.random_split(Cx, Cy, x, tb, y, train_test_split_frac, seed)
         print("Train-test random data split complete")
         if train_valid_rand_split is True:
-            x_train, tb_train, y_train, x_valid, tb_valid, y_valid = \
-                PopeDataProcessor.random_split(x_train, tb_train, y_train, fraction=train_valid_split_frac, seed=seed)
+            Cx_train, Cy_train, x_train, tb_train, y_train, Cx_valid, Cy_valid, x_valid, tb_valid, y_valid = \
+                DataSplitter.random_split(Cx, Cy, x_train, tb_train, y_train, train_valid_split_frac, seed)
             print("Train-valid random data split complete")
         else:
-            x_valid, tb_valid, y_valid = [], [], []
+            Cx_valid, Cy_valid, x_valid, tb_valid, y_valid = [], [], [], [], []
             print("No data allocated for validation")
 
     # Else split database according to specified cases if random split = False
     else:
-        db2case = case_dicts.case_dict_names()
-        case_dict, n_case_points = db2case[database_name]
-        x_train, tb_train, y_train = PopeDataProcessor.specified_split(x, tb, y, train_list, n_case_points, case_dict, num_tensor_basis, seed)
+        Cx_train, Cy_train, x_train, tb_train, y_train = \
+            DataSplitter.specified_split(Cx, Cy, x, tb, y, train_list, case_dict, num_tensor_basis, seed)
         print("Specified data split for training complete")
-        x_test, tb_test, y_test = PopeDataProcessor.specified_split(x, tb, y, test_list, n_case_points, case_dict, num_tensor_basis, seed)
+        Cx_test, Cy_test, x_test, tb_test, y_test = \
+            DataSplitter.specified_split(Cx, Cy, x, tb, y, test_list, case_dict, num_tensor_basis, seed, shuffle=False)
         print("Specified data split for testing complete")
         if train_valid_rand_split is False:
-            x_valid, tb_valid, y_valid = PopeDataProcessor.specified_split(x, tb, y, valid_list, n_case_points, case_dict, num_tensor_basis, seed)
+            Cx_valid, Cy_valid, x_valid, tb_valid, y_valid = \
+                DataSplitter.specified_split(Cx, Cy, x, tb, y, valid_list, case_dict, num_tensor_basis, seed,
+                                             shuffle=False)
             print("Specified data split for validation complete")
         else:
-            x_valid, tb_valid, y_valid = [], [], []
+            Cx_valid, Cy_valid, x_valid, tb_valid, y_valid = [], [], [], [], []
             print("No data allocated for validation")
 
-    return x_train, tb_train, y_train, x_test, tb_test, y_test, x_valid, tb_valid, y_valid
+    return Cx_train, Cy_train, x_train, tb_train, y_train, Cx_test, Cy_test, x_test, tb_test, y_test, \
+        Cx_valid, Cy_valid, x_valid, tb_valid, y_valid
 
 
 def tbnn_ops(x_train, tb_train, y_train, x_valid, tb_valid, y_valid, x_test, tb_test, y_test, batch_size, num_hid_layers,
              num_hid_nodes, af, af_params, seed, weight_init, weight_init_params, loss, optimizer, init_lr, lr_scheduler, lr_scheduler_params, min_epochs,
-              max_epochs, interval, avg_interval, print_freq, log, enforce_realiz, num_realiz_its, num_tensor_basis):
+              max_epochs, interval, avg_interval, print_freq, log, enforce_realiz, num_realiz_its, num_tensor_basis, num_inputs):
 
     # Use GPU if possible
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Prepare batch data in dataloaders and check dataloaders
     dataloader = DataLoader(x_train, tb_train, y_train, x_valid, tb_valid, y_valid, x_test, tb_test, y_test, batch_size)
-    DataLoader.check_data_loaders(dataloader.train_loader, x_train, tb_train, y_train, num_inputs=5,
-                                  num_tensor_basis=num_tensor_basis)
-    DataLoader.check_data_loaders(dataloader.valid_loader, x_valid, tb_valid, y_valid, num_inputs=5,
-                                  num_tensor_basis=num_tensor_basis)
-    DataLoader.check_data_loaders(dataloader.test_loader, x_test, tb_test, y_test, num_inputs=5,
-                                  num_tensor_basis=num_tensor_basis)
+    DataLoader.check_data_loaders(dataloader.train_loader, x_train, tb_train, y_train, num_inputs, num_tensor_basis)
+    DataLoader.check_data_loaders(dataloader.valid_loader, x_valid, tb_valid, y_valid, num_inputs, num_tensor_basis)
+    DataLoader.check_data_loaders(dataloader.test_loader, x_test, tb_test, y_test, num_inputs, num_tensor_basis)
 
     # Prepare TBNN architecture structure and check structure
-    structure = NetworkStructure(num_hid_layers, num_hid_nodes, af, af_params, num_inputs=5,
-                                 num_tensor_basis=num_tensor_basis)
+    structure = NetworkStructure(num_hid_layers, num_hid_nodes, af, af_params, num_inputs, num_tensor_basis)
     structure.check_structure()
     print("TBNN architecture structure and data loaders checked")
 
     # Construct TBNN and perform training, validation and testing
     tbnn = Tbnn(device, seed, structure, weight_init=weight_init, weight_init_params=weight_init_params).double()
     tbnn_tvt = TbnnTVT(loss, optimizer, init_lr, lr_scheduler, lr_scheduler_params, min_epochs, max_epochs, interval,
-                       avg_interval, print_freq, log, model=tbnn)
-    epoch_count, final_train_rmse, final_valid_rmse = tbnn_tvt.fit(device, train_loader=dataloader.train_loader,
-                                                                   valid_loader=dataloader.valid_loader, model=tbnn)
-    y_pred, test_rmse = tbnn_tvt.perform_test(device, enforce_realiz, num_realiz_its, log,
-                                              test_loader=dataloader.test_loader, model=tbnn)
+                       avg_interval, print_freq, log, tbnn)
+    epoch_count, final_train_rmse, final_valid_rmse = tbnn_tvt.fit(device, dataloader.train_loader,
+                                                                   dataloader.valid_loader, tbnn)
+    y_pred, test_rmse = tbnn_tvt.perform_test(device, enforce_realiz, num_realiz_its, log, dataloader.test_loader, tbnn)
 
     return epoch_count, final_train_rmse, final_valid_rmse, y_pred, test_rmse
 
@@ -153,12 +166,19 @@ write_error_means()
 """
 
 
-def trial_iter(n_seeds, x, tb, y, train_list, valid_list, test_list, train_valid_rand_split, train_valid_split_frac,
+def trial_iter(n_seeds, Cx, Cy, x, tb, y, train_list, valid_list, test_list, train_valid_rand_split, train_valid_split_frac,
                train_test_rand_split, train_test_split_frac, num_tensor_basis, num_hid_layers, num_hid_nodes, af,
                af_params, init_lr, lr_scheduler, lr_scheduler_params, weight_init, weight_init_params, max_epochs,
                min_epochs, interval, avg_interval, loss, optimizer, batch_size, enforce_realiz, num_realiz_its,
-               folder_path, user_vars, print_freq, database_name):
-
+               folder_path, user_vars, print_freq, case_dict, num_inputs):
+    """
+    After obtaining x, tb and y from preprocessing, this function is run for a user-specified number of random seeds
+    to train n_seed TBNN instances and obtain n_seed predictions.
+    1) Create new trial folder. A trial is a particular set-up of parameter values on the frontend.py file.
+    2) If seed = 1, initialise TVT error lists and write the parameter values.
+    3) Set the seed for all operations.
+    4)
+    """
     for seed in range(1, n_seeds+1):
         current_folder, log = init_log(folder_path, seed)
         if seed == 1:
@@ -166,20 +186,19 @@ def trial_iter(n_seeds, x, tb, y, train_list, valid_list, test_list, train_valid
             write_param_txt(current_folder, folder_path, user_vars)
             write_param_csv(current_folder, folder_path, user_vars)
         set_seed(seed)
-        x_train, tb_train, y_train, x_test, tb_test, y_test, x_valid, tb_valid, y_valid = \
-            split_database(x=x, tb=tb, y=y, train_list=train_list, valid_list=valid_list, test_list=test_list,
-                           seed=seed, train_valid_rand_split=train_valid_rand_split,
-                           train_test_rand_split=train_test_rand_split, train_valid_split_frac=train_valid_split_frac,
-                           train_test_split_frac=train_test_split_frac, num_tensor_basis=num_tensor_basis,
-                           database_name=database_name)
-        write_test_truth_bij(folder_path, seed, true_bij=y_test)
+        Cx_train, Cy_train, x_train, tb_train, y_train, Cx_test, Cy_test, x_test, tb_test, y_test, \
+            Cx_valid, Cy_valid, x_valid, tb_valid, y_valid = \
+            split_database(Cx, Cy, x, tb, y, train_list, valid_list, test_list, seed, train_valid_rand_split,
+                           train_test_rand_split, train_valid_split_frac, train_test_split_frac, num_tensor_basis,
+                           case_dict)
+        if seed == 1:
+            write_test_truth_bij(Cx_test, Cy_test, folder_path, y_test)
         epoch_count, final_train_rmse, final_valid_rmse, y_pred, test_rmse, = \
             tbnn_ops(x_train, tb_train, y_train, x_valid, tb_valid, y_valid, x_test, tb_test, y_test, batch_size,
                       num_hid_layers, num_hid_nodes, af, af_params, seed, weight_init, weight_init_params, loss,
                       optimizer, init_lr, lr_scheduler, lr_scheduler_params, min_epochs, max_epochs, interval, avg_interval, print_freq, log,
-                      enforce_realiz, num_realiz_its, num_tensor_basis)
-        write_bij_results(folder_path, seed, y_pred, current_folder)
-        # del x_train, tb_train, y_train, x_test, tb_test, y_test, x_valid, tb_valid, y_valid, y_pred
+                      enforce_realiz, num_realiz_its, num_tensor_basis, num_inputs)
+        write_bij_results(Cx_test, Cy_test, folder_path, seed, y_pred, current_folder)
         final_train_rmse_list.append(final_train_rmse)
         final_valid_rmse_list.append(final_valid_rmse)
         test_rmse_list.append(test_rmse)
--- TBNN/preprocessor.py
+++ TBNN/preprocessor.py
@@ -2,7 +2,8 @@ import random
 import numpy as np
 
 
-def load_data(database_name, n_skiprows):
+def load_data(database, num_input_markers, num_zonal_markers=float("nan"), pressure_tf=False, tke_tf=False,
+              input_markers_tf=False, zonal_markers_tf=False):
     """
     Load CFD data for TBNN calculations.
     :param database_name: Name of the data file containing the CFD data. The columns must be in the following order: TKE
@@ -12,47 +13,83 @@ def load_data(database_name, n_skiprows):
     high-fidelity simulation.
     """
 
-    # Load in database and separate data
-    data = np.loadtxt(database_name, skiprows=n_skiprows)
-    k = data[:, 0]
-    eps = data[:, 1]
-    grad_u_flat = data[:, 2:11]
-    stresses_flat = data[:, 11:]
+    # Var  Cx | Cy | k | eps | gradU | gradp |  U  | gradk |   marker inputs   |  zonal marker  |tauij|
+    # Col   0    1   2    3     4-12   13-15  16-18  19-21   -(nm+nz+9):-(nz+9)    -(nz+9):-9     -9:
+
+    # Define compulsory data
+    Cx = database[:, 0]
+    Cy = database[:, 1]
+    k = database[:, 2]
+    eps = database[:, 3]
+    grad_u_flat = database[:, 4:13]
+    tauij_flat = database[:, -9:]
+
+    # Define grad_p data
+    if pressure_tf is True:
+        grad_p = database[:, 13:16]
+        u = database[:, 16:19]
+    else:
+        grad_p = float("nan")
+        u = float("nan")
+
+    # Define grad_k data
+    if pressure_tf is False and tke_tf is True:
+        grad_k = database[:, 13:16]
+    elif pressure_tf is True and tke_tf is True:
+        grad_k = database[:, 19:22]
+    else:
+        grad_k = float("nan")
+
+    # Define input markers data
+    if input_markers_tf is True and zonal_markers_tf is False:
+        input_markers = database[:, -(num_input_markers+9):-9]
+    elif input_markers_tf is True and zonal_markers_tf is True:
+        input_markers = database[:, -(num_input_markers+num_zonal_markers+9):-(num_zonal_markers+9)]
+    else:
+        input_markers = float("nan")
 
     # Reshape grad_u and stresses to num_points X 3 X 3 arrays
-    num_points = data.shape[0]
+    num_points = database.shape[0]
     grad_u = np.zeros((num_points, 3, 3))
-    stresses = np.zeros((num_points, 3, 3))
+    tauij = np.zeros((num_points, 3, 3))
     for i in range(3):
         for j in range(3):
-            grad_u[:, i, j] = grad_u_flat[:, i*3+j]
-            stresses[:, i, j] = stresses_flat[:, i*3+j]
+            grad_u[:, i, j] = grad_u_flat[:, i*3+j] #TO DO: Check
+            tauij[:, i, j] = tauij_flat[:, i*3+j] #TO DO: Check
 
-    return k, eps, grad_u, stresses
+    return Cx, Cy, k, eps, grad_u, grad_p, u, grad_k, input_markers, tauij
 
 
-class DataProcessor:
+def scalar_basis_manager(data_processor, k, eps, grad_u, rho, u, grad_p, grad_k, incl_p_invars=False,
+                         incl_tke_invars=False):
+
+    Sij, Rij = data_processor.calc_Sij_Rij(grad_u, k, eps)  # Mean strain rate tensor Sij = k/eps*Sij,
+    # mean rotation rate tensor k/eps*Rij
+
+    if incl_p_invars is True:
+        Ap = data_processor.calc_Ap(grad_p, rho, u, grad_u)
+    elif incl_tke_invars is True:
+        Ak = data_processor.calc_Ak(grad_k, eps, k)
+
+    if incl_p_invars is False and incl_tke_invars is False:
+        x = data_processor.calc_scalar_basis(Sij, Rij, pressure=False, tke=False)
+    elif incl_p_invars is True and incl_tke_invars is False:
+        x = data_processor.calc_scalar_basis(Sij, Rij, Ap=Ap, pressure=True, tke=False)
+    elif incl_p_invars is False and incl_tke_invars is True:
+        x = data_processor.calc_scalar_basis(Sij, Rij, Ak=Ak, pressure=False, tke=True)
+    elif incl_p_invars is True and incl_tke_invars is True:
+        x = data_processor.calc_scalar_basis(Sij, Rij, Ap=Ap, Ak=Ak, pressure=True, tke=True)
+
+    return Sij, Rij, x
+
+
+class DataSplitter:
     """
-    Parent class for data processing
+    Class for splitting the entire database into training, validation and testing databases
     """
-    def __init__(self):
-        self.mu = None
-        self.std = None
-
-    def calc_scalar_basis(self, Sij, Rij, is_train=False, *args, **kwargs):
-        if is_train is True or self.mu is None or self.std is None:
-            print("Re-setting normalization constants")
 
     @staticmethod
-    def calc_tensor_basis(Sij, Rij, *args, **kwargs):
-        pass
-
-    @staticmethod
-    def calc_output(tauij):
-        return tauij
-
-    @staticmethod
-    def random_split(inputs, tb, outputs, fraction=0.8, shuffle=True, seed=None):
+    def random_split(Cx, Cy, inputs, tb, outputs, fraction, seed, shuffle=True):
         """
         Randomly splits CFD data into training and test set
         :param inputs: scalar invariants
@@ -67,15 +104,15 @@ class DataProcessor:
         assert 0 <= fraction <= 1, "Fraction must be a real number between 0 and 1"
         num_train = int(fraction*num_points)
         idx = list(range(num_points))
-        DataProcessor.shuffle_idx(seed, idx, shuffle)
+        DataSplitter.shuffle_idx(seed, idx, shuffle)
         train_idx = idx[:num_train]
         test_idx = idx[num_train:]
 
-        return inputs[train_idx, :], tb[train_idx, :, :], outputs[train_idx, :], \
-               inputs[test_idx, :], tb[test_idx, :, :], outputs[test_idx, :]
+        return Cx[train_idx], Cy[train_idx], inputs[train_idx, :], tb[train_idx, :, :], outputs[train_idx, :], \
+            Cx[test_idx], Cy[test_idx], inputs[test_idx, :], tb[test_idx, :, :], outputs[test_idx, :]
 
     @staticmethod
-    def specified_split(inputs, tensor_basis, outputs, case_list, n_case_points, case_dict, num_tensor_basis, seed,
+    def specified_split(Cx, Cy, inputs, tensor_basis, outputs, case_list, case_dict, num_tensor_basis, seed,
                         shuffle=True):
         """
         Splits the x, tb and y arrays into train and test sets using the specified train and test cases
@@ -83,7 +120,6 @@ class DataProcessor:
         :param tensor_basis: tensor basis
         :param outputs: Reynolds stress anisotropy
         :param case_list: list of cases for extraction
-        :param n_case_points: Number of data points per CFD case
         :param case_dict: dictionary for extracting case index
         :return: inputs, tb and outputs for the training and test sets
         """
@@ -95,17 +131,22 @@ class DataProcessor:
 
         # Loop through cases in list to extract x, tb and y
         for case in case_list:
-            extract_rows = list(range(0, case_dict[case]*n_case_points))
-            x = np.concatenate((x, inputs[extract_rows[-n_case_points:], :]), axis=0)
-            tb = np.concatenate((tb, tensor_basis[extract_rows[-n_case_points:], :, :]), axis=0)
-            y = np.concatenate((y, outputs[extract_rows[-n_case_points:], :]), axis=0)
+            keys = [key for key in case_dict.keys() if key <= case]
+            end_row = 0
+            for key in keys:
+                end_row += case_dict[key]
+
+            extract_rows = list(range(0, end_row))
+            x = np.concatenate((x, inputs[extract_rows[-case_dict[case]:], :]), axis=0)
+            tb = np.concatenate((tb, tensor_basis[extract_rows[-case_dict[case]:], :, :]), axis=0)
+            y = np.concatenate((y, outputs[extract_rows[-case_dict[case]:], :]), axis=0)
 
         num_points = x.shape[0]
         assert x.shape[0] == tb.shape[0] == y.shape[0], "Mismatch in number of data points"
         idx = list(range(num_points))
-        DataProcessor.shuffle_idx(seed, idx, shuffle)
+        DataSplitter.shuffle_idx(seed, idx, shuffle)
 
-        return x[idx, :], tb[idx, :, :], y[idx, :]
+        return Cx[idx], Cy[idx], x[idx, :], tb[idx, :, :], y[idx, :]
 
     @staticmethod
     def shuffle_idx(seed, idx, shuffle=True):
@@ -115,4 +156,3 @@ class DataProcessor:
             random.shuffle(idx)
 
         return idx
-
