83f6cdaf4c3bbd995fbf347511b15ae938804815
AnthonyCHM <anthony.man@postgrad.manchester.ac.uk>
2023-03-06 12:48:53 +0000

    PoF Oct 2022 v2.0

---
+++ Cross_validation/FBFS5/csv_combine/TBNN_hp_tuning_csv_combine.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+current_dir = os.getcwd()
+results_all = pd.read_csv(os.path.join(current_dir, 'TBNN_hp_tuning_all_results.csv'))
+
+for i in range(1, 145):
+    job_csv = pd.read_csv(os.path.join(current_dir, 'Job' + str(i), 'TBNN output data',
+                                       'Results', 'Trial_parameters_and_means.csv'))
+    results_all = pd.concat([results_all, job_csv])
+results_all.to_csv(os.path.join(current_dir, 'TBNN_hp_tuning_all_results.csv'),
+                   index=False)
