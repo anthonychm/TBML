671641882a6c20e1ffdc9ff31f86cb58617dfa80
AnthonyCHM <anthony.man@postgrad.manchester.ac.uk>
2023-04-17 10:04:36 +0100

    Apr 2023 finish calc zonal marker

---
+++ Driver/zonal_marker_calculator.py
@@ -0,0 +1,153 @@
+"""
+This script calculates values of zonal markers across the entire flow domain.
+Version 1 (for Apr 2023)
+"""
+
+import numpy as np
+
+
+def main():
+    # Choose CFD case from: "BUMP_h20" "BUMP_h26" "BUMP_h31", "BUMP_h38", "BUMP_h42", "CBFS_13700",
+    # "CNDV_12600", "CNDV_20580", "PHLL_case_0p5", "PHLL_case_0p8", "PHLL_case_1p0", "PHLL_case_1p2", "PHLL_case_1p5"
+    # "DUCT_1100", "DUCT_1150", "DUCT_1250", "DUCT_1300", "DUCT_1350", "DUCT_1400", "DUCT_1500", "DUCT_1600"
+    # "DUCT_1800", "DUCT_2000", "DUCT_2205", "DUCT_2400" "DUCT_2600", "DUCT_2900", "DUCT_3200", "DUCT_3500"
+    # "FBFS_1800", "FBFS_3600", "FBFS_4500", "FBFS_5400", "FBFS_7200"
+    case = "CNDV_20580"
+
+    parent_path = get_parent_path(case)
+    S, R, k, Ux, Uy, Uz, wall_dist = load_marker_data_apr2023(case, parent_path)  # ✓
+    nu = get_nu(case)
+    nd_Q = calc_nd_Q(S, R)
+    nd_TI = calc_nd_TI(k, Ux, Uy, Uz)
+    nd_Ux = calc_nd_Ux(Ux, Uy, Uz)
+    Re_y = calc_Re_y(k, wall_dist, nu)
+    write_vars(case, vars(), "nd_Q", "nd_TI", "nd_Ux", "Re_y")  # ✓
+    return nd_Q, nd_TI, nd_Ux, Re_y
+
+
+def get_parent_path(case):
+    # Get parent directory path for the given case ✓
+    if "FBFS" in case:
+        parent_path = "C:/Users/h81475am/Dropbox (The University of Manchester)/PhD_Anthony_Man/ML Database/FBFS"
+    elif any(name in case for name in ["BUMP", "CBFS", "CNDV", "PHLL", "DUCT"]):
+        parent_path = ("C:/Users/h81475am/Dropbox (The University of Manchester)/PhD_Anthony_Man/"
+                       "AM_PhD_shared_documents/McConkey data")
+    else:
+        raise Exception("No matching parent path for this case")
+    return parent_path
+
+
+def create_var_dict(var_list, parent_path, child_path, case):
+    # Create dictionary of variables for calculating the zonal markers ✓
+    var_dict = {}
+    for count, var in enumerate(var_list):
+        try:
+            var_dict[var] = np.load(parent_path + child_path + case + "_" + var + ".npy")
+        except:
+            var_dict[var] = np.loadtxt(parent_path + child_path + case + "_" + var + ".txt")
+    return var_dict
+
+
+def load_marker_data_apr2023(case, parent_path):
+    # Load data for calculating zonal markers ✓
+    var_list = ["S", "R", "k", "Ux", "Uy", "Uz", "wall_dist"]
+    if "FBFS" in case:
+        var_dict = create_var_dict(var_list, parent_path, "/" + case + "/", case)
+    elif any(name in case for name in ["BUMP", "CBFS", "CNDV", "PHLL", "DUCT"]):
+        turb_model = "komegasst"
+        var_dict = create_var_dict(var_list, parent_path, "/" + turb_model + "/" + turb_model + "_", case)
+    else:
+        raise Exception("No matching data for this case")
+    return var_dict["S"], var_dict["R"], var_dict["k"], var_dict["Ux"], var_dict["Uy"], var_dict["Uz"], \
+        var_dict["wall_dist"]
+
+
+def get_nu(case):
+    # Get kinematic viscosity, nu for a specified case ✓
+    nu_dict = {
+        "PHLL": 5e-6,  # ✓
+        "BUMP": 2.53e-5,  # ✓
+        "CNDV_12600": 7.94e-5,  # ✓
+        "CNDV_20580": 4.86e-5,  # ✓
+        "CBFS": 7.3e-5,  # ✓
+        "FBFS": 1e-5  # ✓
+    }
+
+    for key in nu_dict:
+        if key in case:
+            nu = nu_dict[key]
+            return nu
+
+
+def calc_S_and_R(parent_path, child_path, case):
+    # Calculate dimensional mean strain rate S and mean rotation rate R, both with dimensions 1/s ✓
+
+    # Convert grad_u_flat to 3-dimensional format
+    grad_u_flat = np.loadtxt(parent_path + child_path + case + "_gradU.txt")
+    num_points = grad_u_flat.shape[0]
+    grad_u = np.full((num_points, 3, 3), np.nan)
+    for i in range(3):
+        for j in range(3):
+            grad_u[:, i, j] = grad_u_flat[:, (3*i)+j]
+
+    # Calculate S and R
+    S = np.full((num_points, 3, 3), np.nan)
+    R = np.full((num_points, 3, 3), np.nan)
+    for i in range(num_points):
+        S[i, :, :] = 0.5 * (grad_u[i, :, :] + np.transpose(grad_u[i, :, :]))
+        R[i, :, :] = 0.5 * (grad_u[i, :, :] - np.transpose(grad_u[i, :, :]))
+
+    # Write S and R as npy files
+    np.save(str(case) + '_S.npy', S)
+    np.save(str(case) + '_R.npy', R)
+
+
+def calc_nd_Q(S, R):
+    # Calculate non-dimensional Q-criterion, nd_Q = (||R||^2 - ||S||^2)/(||R||^2 + ||S||^2) ✓
+    nd_Q = []
+    for row in range(S.shape[0]):
+        norm_S = 0
+        norm_R = 0
+        for i in range(2):
+            for j in range(2):
+                norm_S += S[row, i, j] ** 2
+                norm_R += R[row, i, j] ** 2
+        norm_S = np.sqrt(norm_S)
+        norm_R = np.sqrt(norm_R)
+        nd_Q_tmp = ((norm_R ** 2) - (norm_S ** 2))/((norm_R ** 2) + (norm_S ** 2))
+        nd_Q.append(nd_Q_tmp)
+    return nd_Q
+
+
+def calc_nd_TI(k, Ux, Uy, Uz):
+    # Calculate non-dimensional turbulence intensity, nd_TI = k/(0.5UiUi + k) ✓
+    nd_TI = k/((0.5*((Ux ** 2)+(Uy ** 2)+(Uz ** 2)))+k)
+    return nd_TI
+
+
+def calc_nd_Ux(Ux, Uy, Uz):
+    # Calculate non-dimensional streamwise velocity, nd_Ux = Ux/mag(U) ✓
+    nd_Ux = Ux/np.sqrt((Ux ** 2)+(Uy ** 2)+(Uz ** 2))
+    return nd_Ux
+
+
+def calc_Re_y(k, wall_dist, nu):
+    # Calculate turbulence Reynolds number, Re_y = sqrt(k)*y/200nu ✓
+    Re_y = (np.sqrt(k)*wall_dist)/(200*nu)
+    return Re_y
+
+
+def write_vars(case, vars_dict, *args):
+    # Write results as a .txt file ✓
+    for var in args:
+        np.savetxt(case + "_" + var + ".txt", vars_dict[var])
+
+# def write_vars(var_dict, case):
+#     # Write marker results as a .txt file
+#     for key in var_dict:
+#         np.savetxt(case + "_" + key + ".txt", var_dict[key])
+
+
+if __name__ == "__main__":
+    main()
+    print("finish")
