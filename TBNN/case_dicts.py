4d50a7d32086aa38f2f6b403051774410d533f77
AnthonyCHM <anthony.man@postgrad.manchester.ac.uk>
2024-09-19 15:37:20 +0100

    19/09/2024 include chan7_no_oti

--- TBNN/case_dicts.py
+++ TBNN/case_dicts.py
@@ -1,15 +1,16 @@
 def case_dict_names():
     database2case = {
-        "CHAN7_database.txt": channel_flow_7(),
-        "FBFS_full_set_no_walls.txt": fbfs_3(),
-        "FBFS5_full_set_no_walls.txt": fbfs_5(),
-        "FBFS5_IMPJ20000_dataset.txt": fbfs_5_impj_20000(),
-        "PHLL4_dataset.txt": phll_4()
+        "CHAN7_database.txt": chan7(),
+        "FBFS_full_set_no_walls.txt": fbfs3(),
+        "FBFS5_full_set_no_walls.txt": fbfs5(),
+        "FBFS5_IMPJ20000_dataset.txt": fbfs5_impj20000(),
+        "PHLL4_dataset.txt": phll4(),
+        "CHAN7_no_oti_half_database.txt": chan7_no_oti()
     }
     return database2case
 
 
-def channel_flow_7():
+def chan7():
     keys = [180, 290, 395, 490, 590, 760, 945]
     num_points = 182
     case_dict = dict.fromkeys(keys, num_points)
@@ -17,7 +18,7 @@ def channel_flow_7():
     return case_dict, num_points, num_skip_rows
 
 
-def fbfs_3():
+def fbfs3():
     keys = [1, 2, 4]
     num_points = 80296
     case_dict = dict.fromkeys(keys, num_points)
@@ -25,7 +26,7 @@ def fbfs_3():
     return case_dict, num_points, num_skip_rows
 
 
-def fbfs_5():  # ✓
+def fbfs5():  # ✓
     keys = [1, 2, 2.5, 3, 4]
     num_points = 80296
     case_dict = dict.fromkeys(keys, num_points)
@@ -33,7 +34,7 @@ def fbfs_5():  # ✓
     return case_dict, num_points, num_skip_rows
 
 
-def fbfs_5_impj_20000():
+def fbfs5_impj20000():
     keys = [1, 2, 2.5, 3, 4, 20000]
     num_points = [80296, 80296, 80296, 80296, 80296, 112006]
     case_dict = dict(zip(keys, num_points))
@@ -41,9 +42,17 @@ def fbfs_5_impj_20000():
     return case_dict, num_points, num_skip_rows
 
 
-def phll_4():
+def phll4():
     keys = [0.5, 1, 1.2, 1.5]
     num_points = 14751
     case_dict = dict.fromkeys(keys, num_points)
+    num_skip_rows = 1
+    return case_dict, num_points, num_skip_rows
+
+
+def chan7_no_oti():
+    keys = [180, 290, 395, 490, 590, 760, 945]
+    num_points = [56, 56, 55, 54, 54, 53, 53]
+    case_dict = dict(zip(keys, num_points))
     num_skip_rows = 1
     return case_dict, num_points, num_skip_rows
