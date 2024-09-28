c64c20fdabe0d02681b4ae6dcdc683352d9dd98d
AnthonyCHM <anthony.man@postgrad.manchester.ac.uk>
2024-09-28 10:41:32 +0100

    28/09/2024 created chan_bij_plotter.py and chan_plotter_core.py

---
+++ ML_post/chan_bij_plotter.py
@@ -0,0 +1,99 @@
+"""
+This script plots line profiles of bij predicted by ML vs. RANS SST vs. LES in channel
+flow cases.
+
+Test 3: [TBNN, TBmix]
+Test 4: one-to-two TBmix
+"""
+
+import matplotlib.pyplot as plt
+import chan_plotter_core as cpc
+
+
+def main():
+    # Specify ML results identifiers e.g. [first profile, second profile, etc]
+    trial = [46]  # [27, 46]
+    seed = [1]  # [1, 1]
+    num_kernels = [2]  # [None, 2]
+    model = ["TBmix"]  # ["TBNN", "TBmix"]
+    Re = 395
+
+    # Specify x and y axes
+    dim_var_name = "y+"
+    bij_comp = "b12"
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+    # Load RANS and LES results for comparison
+    cmp_loader = cpc.ResultsLoader(locals())
+    rans_result, true_result = cmp_loader.load_cmp_results()
+    rans_Cy = cmp_loader.extract_chan_coords(rans_result)
+    true_Cy = cmp_loader.extract_chan_coords(true_result)
+    rans_bij, true_bij = cmp_loader.extract_cmp_bij(rans_result, true_result)
+
+    # Preprocess RANS plotting data
+    rans_plotter = cpc.ChanLinePlotter(locals(), rans_Cy, bij=rans_bij)
+    rans_plotter.calc_yplus()
+    rans_plotter.halve_chan(y_var=rans_plotter.y_var, bij=rans_plotter.bij)
+
+    # Preprocess LES plotting data
+    true_plotter = cpc.ChanLinePlotter(locals(), true_Cy, bij=true_bij)
+    true_plotter.calc_yplus()
+    true_plotter.halve_chan(y_var=true_plotter.y_var, bij=true_plotter.bij)
+
+    # Initialise plotting
+    plt.figure(figsize=(3.5, 1.75))
+    bij_comp_dict = {"b11": 0, "b12": 1, "b13": 2, "b22": 4, "b23": 5, "b33": 8}
+    col_idx = bij_comp_dict[bij_comp]
+    # true_plotter.fill_oti_subdomain(plt, ymin, ymax)
+
+    # Plot RANS and LES bij results
+    plt.plot(rans_plotter.y_var, rans_plotter.bij[:, col_idx], 'r',
+             linewidth=1, label="RANS")
+    plt.plot(true_plotter.y_var, true_plotter.bij[:, col_idx], 'g', linewidth=1,
+             label="LES")
+
+    # Recursively load and plot ML results
+    for ml_count in range(len(trial)):
+        ml_loader = cpc.ResultsLoader(locals())
+        ml_loader.get_identifiers(ml_count)
+        ml_loader.declare_path()
+
+        # Extract coordinates and predictions of bij from ML
+        if ml_loader.model == "TBNN":
+            ml_result = ml_loader.load_tbnn_results()
+            ml_Cy, ml_bij = ml_result[:, 0], ml_result[:, 1:]
+        elif ml_loader.model == "TBmix":
+            mu_bij_all, sigma = ml_loader.load_tbmix_results()
+            ml_Cy, ml_bij = sigma[:, 0], None  # ml_bij set to None as mu_bij_all is used
+        else:
+            raise Exception("Invalid model name")
+
+        # Preprocess ML plotting data
+        if ml_loader.model == "TBNN":
+            ml_plotter = cpc.ChanLinePlotter(locals(), ml_Cy, bij=ml_bij)
+            ml_plotter.calc_yplus()
+            ml_plotter.halve_chan(y_var=ml_plotter.y_var, bij=ml_plotter.bij)
+        elif ml_loader.model == "TBmix":
+            ml_plotter = cpc.ChanLinePlotter(locals(), ml_Cy, bij=ml_bij, mix_bij=True)
+            ml_plotter.calc_yplus()
+            ml_plotter.halve_chan(y_var=ml_plotter.y_var,
+                                  mu_bij_all=ml_plotter.mu_bij_all,
+                                  sigma=ml_plotter.sigma)
+        else:
+            raise Exception("Invalid model name")
+
+        # Plot bij results from ML
+        if ml_loader.model == "TBNN":
+            plt.plot(ml_plotter.y_var, ml_plotter.bij[:, col_idx], 'k', linewidth=1,
+                     label=ml_loader.model)
+        elif ml_loader.model == "TBmix":
+            mu = ml_plotter.extract_mu_bij_comp(col_idx)
+            ml_plotter.plot_kernel_bij(plt, mu)
+
+    # Format bij plot
+    true_plotter.fmt_bij_plot(plt, dim_var_name, bij_comp)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ML_post/chan_plotter_core.py
@@ -0,0 +1,167 @@
+"""
+This script contains classes used in chan_bij_plotter.py and chan_mix_coeff_plotter.py.
+"""
+
+import numpy as np
+import sys
+sys.path.append('../TBNN')
+from TBNN.calculator import PopeDataProcessor
+
+
+class ResultsLoader:
+    def __init__(self, locals_dict):
+        self.trial = locals_dict['trial']
+        self.seed = locals_dict['seed']
+        self.Re = locals_dict['Re']
+        self.model = locals_dict['model']
+        self.bij_comp = locals_dict['bij_comp']
+        self.num_kernels = locals_dict['num_kernels']
+        self.path = None
+
+    def get_identifiers(self, count):
+        # Get identifiers (trial no., seed no., model type) for the current ML result
+        self.trial = self.trial[count]
+        self.seed = self.seed[count]
+        self.model = self.model[count]
+        if self.model == "TBmix":
+            self.num_kernels = self.num_kernels[count]
+
+    def declare_path(self):
+        # Specify folder path of ML model results
+        self.path = "../" + self.model + "/Saved results/MDN Feb 2024 CHAN trials/Trial " \
+                    + str(self.trial) + "/Trial" + str(self.trial) + "_seed" + \
+                    str(self.seed) + "_"
+
+    def load_tbnn_results(self):
+        # Load tbnn results - these are deterministic
+        assert self.model == "TBNN"
+        return np.loadtxt(self.path + "TBNN_test_prediction_bij.txt", skiprows=1)
+
+    def load_tbmix_results(self):
+        # Load tbmix component results
+        assert self.model == "TBmix"
+
+        # Standard deviation
+        sigma = np.loadtxt(self.path + "sigma.txt", skiprows=1)
+
+        # Mean bij from each kernel
+        mu_bij_all = np.full((self.num_kernels, len(sigma), 10), np.nan)
+        for k in range(self.num_kernels):
+            mu_bij_all[k, :, :] = np.loadtxt(self.path + "mu_bij_from_kernel_" +
+                                             str(k+1) + ".txt", skiprows=1)
+
+        return mu_bij_all, sigma
+
+    @staticmethod
+    def load_cmp_results():
+        # Load RANS and LES results for comparison
+        rans_result = np.loadtxt("CHAN7/CHAN7_rans_ref_table.txt", skiprows=1)
+        true_result = np.loadtxt("../Datasets/CHAN7_dataset.txt", skiprows=1)
+        return rans_result, true_result
+
+    def calc_start_end(self):
+        # Extract start and end rows corresponding to the Re number
+        Re_numbers = [180, 290, 395, 490, 590, 760, 945]
+        Re_idx = Re_numbers.index(self.Re)
+        num_points = 182
+        return Re_idx * num_points, (Re_idx + 1) * num_points
+
+    def extract_chan_coords(self, result):
+        # Extract Cy coordinates
+        start_row, end_row = self.calc_start_end()
+        return result[start_row:end_row, 0]
+
+    def extract_cmp_bij(self, rans_result, true_result):
+        # Extract array containing only bij from RANS and LES
+        rans_bij = rans_result[:, 1:10]
+        true_tauij = true_result[:, -9:]
+        true_bij = PopeDataProcessor.calc_true_output(true_tauij, output_var="bij")
+        start_row, end_row = self.calc_start_end()
+        return rans_bij[start_row:end_row, :], true_bij[start_row:end_row, :]
+
+
+class ChanLinePlotter:
+    def __init__(self, locals_dict, Cy, bij=None, mix_bij=False):
+        self.Re = locals_dict['Re']
+        self.Re_list = [180, 290, 395, 490, 590, 760, 945]
+        self.Cy = Cy
+        self.y_var = None
+        self.bij = bij
+        self.mix_coeffs = None
+
+        if mix_bij is True:
+            self.mu_bij_all = locals_dict['mu_bij_all'][:, :, 1:]
+            self.sigma = locals_dict['sigma'][:, 1:]
+            self.num_kernels = self.mu_bij_all.shape[0]
+
+    def calc_yplus(self):
+        # Calculate y+
+        tau_w_list = [0.02030, 0.05107, 0.09313, 0.14541, 0.20732, 0.35267, 0.52228]
+        tau_w_dict = dict(zip(self.Re_list, tau_w_list))
+        utau, nu = np.sqrt(tau_w_dict[self.Re]), 1.568e-05
+        self.y_var = utau * self.Cy / nu
+
+    def halve_chan(self, **kwargs):
+        # Keep results from the first half of the channel (Cy = 0 to 0.02)
+        end_row = round(len(self.y_var) / 2)
+        for key, arr in kwargs.items():
+            if arr.ndim == 1:
+                setattr(self, key, arr[0:end_row])
+            elif arr.ndim == 2:
+                setattr(self, key, arr[0:end_row, :])
+            elif arr.ndim == 3:
+                setattr(self, key, arr[:, 0:end_row, :])
+            else:
+                raise Exception("Number of dimensions not supported")
+
+    def extract_mu_bij_comp(self, col_idx):
+        # Extract required component of mean bij
+        mu = np.full((len(self.y_var), self.num_kernels), np.nan)
+        for k in range(self.num_kernels):
+            mu[:, k] = self.mu_bij_all[k, :, col_idx]
+        return mu
+
+    def plot_kernel_bij(self, plt, mu):
+        # Plot kernel bij results from TBmix
+        k_count = 0
+        for mu_k, sigma_k in zip(mu.T, self.sigma.T):
+            k_count += 1
+            plt.plot(self.y_var, mu_k, linewidth=1, label="kernel " + str(k_count))
+            plt.fill_between(self.y_var, mu_k + sigma_k, mu_k - sigma_k, alpha=0.3)
+
+    def fill_oti_subdomain(self, plt, ymin, ymax):
+        # Fill the y+ range containing one-to-interval relation between alpha and b12
+        # where alpha (= (k/eps)*(du/dy)) < 3.4
+
+        # Specify y+ range containing the one-to-interval region
+        yplus_oti = [[], [], [], [], [], [], []]
+        yplus_oti_dict = dict(zip(self.Re_list, yplus_oti))
+
+        # Plot rectangular fill representing one-to-interval region
+        left, right = yplus_oti_dict[self.Re][0], yplus_oti_dict[self.Re][1]
+        plt.Rectangle((left, ymin), right-left, ymax-ymin, facecolor="red", alpha=0.1)
+
+        # Plot bounding lines around rectangle
+        plt.plot([left, left], [ymin, ymax], 'k--')
+        plt.plot([right, right], [ymin, ymax], 'k--')
+
+    def fmt_bij_plot(self, plt, xlabel, ylabel):
+        # Format bij plot
+        plt.xlim(min(self.y_var), max(self.y_var))
+        plt.ylim(-0.175, 0)
+        plt.xscale('log')
+        plt.tick_params(labelsize=6)
+        # plt.legend(loc='upper center')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.show()
+
+    def fmt_mix_coeffs_plot(self, plt, xlabel):
+        # Format mixture coefficients plot
+        plt.xlim(min(self.y_var), max(self.y_var))
+        plt.ylim(0, 1)
+        plt.tick_params(labelsize=8)
+        plt.legend(loc='lower right')
+        plt.xlabel(xlabel)
+        plt.ylabel("Prior probability")
+        plt.show()
